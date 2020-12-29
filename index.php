9780b3957bd85b648e2f24c66fffff41e4c22920
Donny Rizal Adhi Pratama <44853905+L200183161@users.noreply.github.com>
2020-12-29 20:14:41 +0700

    nambah root dicoba dulu smpk

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>ini namanya root</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="jumbotron">
+        <h1 class="display-3">root</h1>
+        <p class="lead">Pindah ke lain link</p>
+        <hr class="my-2">
+        <p>Tolong dengan sangat membuka rootnya</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="modul1_L2001836161_X/" role="button">Modul 1</a>
+            <a class="btn btn-primary btn-lg" href="modul2_L2001836161_X/" role="button">Modul 2</a>
+            <a class="btn btn-primary btn-lg" href="modul3_L2001836161_X/" role="button">Modul 3</a>
+            <a class="btn btn-primary btn-lg" href="modul4_L2001836161_X/" role="button">Modul 4</a>
+            <a class="btn btn-primary btn-lg" href="modul5_L2001836161_X/" role="button">Modul 5</a>
+            <a class="btn btn-primary btn-lg" href="modul6_L2001836161_X/" role="button">Modul 6</a>
+            <a class="btn btn-primary btn-lg" href="modul7_L2001836161_X/" role="button">Modul 7</a>
+            <a class="btn btn-primary btn-lg" href="modul8_L2001836161_X/" role="button">Modul 8</a>
+            <a class="btn btn-primary btn-lg" href="modul9_L2001836161_X/" role="button">Modul 9</a>
+            <a class="btn btn-primary btn-lg" href="modul10_L2001836161_X/" role="button">Modul 10</a>
+            <a class="btn btn-primary btn-lg" href="Quiz/" role="button">Quiz</a>
+            <a class="btn btn-primary btn-lg" href="UTS/" role="button">UTS</a>
+        </p>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
